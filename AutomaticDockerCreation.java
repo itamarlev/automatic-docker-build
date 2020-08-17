a6e0ee43fb1c48b462553c7216ed05993f4f9ac2
Itamar Lev <itamar.lev@nokia.com>
2020-08-17 17:10:16 +0300

    another line add

--- AutomaticDockerCreation.java
+++ AutomaticDockerCreation.java
@@ -1,7 +1,7 @@
 public class AutomaticDockerCreation {
     public static void main(String args[]){
-        int i = 0;
-	    System.out.println("hi, I've been created automatically");
+          System.out.println("hi, I've been created automatically");
+	  System.out.println("this is a new line");
     }
 }
 
